393403ca658b12dc7943e8dbab8b15682a65c52d
FildasPA <julien.boge@alumni.univ-avignon.fr>
2016-11-02 21:08:01 +0100

    Rework layout

--- birdy/layout/layout.php
+++ birdy/layout/layout.php
@@ -1,13 +1,16 @@
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Index</title>
-  </head>
-
-  <body>
-    <h2>Super c'est ton appli ! </h2>
-    <?php include($template_view); ?>
-  </body>
-
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Birdy</title>
+  <link rel="stylesheet" href="css/main.css">
+  <script src="main.js"></script>
+</head>
+<body>
+	<div id="main-container">
+		<h1>Index</h1>
+		<?php include($template_view); ?>
+		<p><a href="birdy.php?action=sign">Inscription</a></p>
+	</div>
+</body>
 </html>
