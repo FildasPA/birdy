8b023a0dec382ac534d5137c62194b6d043cb498
FildasPA <julien.boge@alumni.univ-avignon.fr>
2016-12-06 15:04:44 +0100

    [View] Fix users list link

--- birdy/view/displayUsersSuccess.php
+++ birdy/view/displayUsersSuccess.php
@@ -9,7 +9,7 @@
 <?php
 
 foreach($context->users as $user) {
-	echo "<tr class='user' onclick=\"window.document.location=birdy.php?action=viewProfile&login='".$user->identifiant."\">";
+	echo "<tr class='user' onclick=\"window.location.href='birdy.php?action=viewProfile&login=" . $user->identifiant . "'\">";
 	echo "<td>".$user->identifiant."</td>";
 	echo "<td>".$user->nom."</td>";
 	echo "<td>".$user->prenom."</td>";
