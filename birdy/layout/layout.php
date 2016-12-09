2374e7af71e9840f6661f818c7c63f9dfd3b4788
FildasPA <julien.boge@alumni.univ-avignon.fr>
2016-12-09 13:02:12 +0100

    Clean layout, separate links into another file

--- birdy/layout/layout.php
+++ birdy/layout/layout.php
@@ -8,16 +8,7 @@
 <body>
 	<div id="main-container">
 		<?php include($template_view); ?>
-		<ul>
-			<li><a href="birdy.php?action=index">Index</a></li>
-			<li><a href="birdy.php?action=register">Inscription</a></li>
-			<li><a href="birdy.php?action=login">Connexion</a></li>
-			<li><a href="birdy.php?action=displayUsers">Liste utilisateur</a></li>
-			<li><a href="birdy.php?action=superTest&par1=mdr&par2=lol">SuperTest</a></li>
-			<li><a href="birdy.php?action=helloWorld">helloWorld</a></li>
-			<li><a href="birdy.php?action=logout">Déconnexion</a></li>
-		</ul>
-
+		<?php include($nameApp . "/view/links.php"); ?>
 	</div>
 </body>
 </html>
