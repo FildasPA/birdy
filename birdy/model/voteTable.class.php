bdd687768f61ebac472ddc552b1984b0acf89cc7
FildasPA <julien.boge@alumni.univ-avignon.fr>
2017-01-04 18:23:00 +0100

    [Model] Vote & voteTable

---
+++ birdy/model/voteTable.class.php
@@ -0,0 +1,27 @@
+<?php
+
+//=============================================================================
+// ▼ Table Vote
+// ----------------------------------------------------------------------------
+//
+//=============================================================================
+class voteTable extends baseTable
+{
+	public static $objectType = "vote";
+	public static $tableName  = "vote";
+
+	//---------------------------------------------------------------------------
+	// * Get post by user id and tweet id
+	//---------------------------------------------------------------------------
+	public static function getVoteByUserIdAndTweetId($userId,$tweetId)
+	{
+		$sql = "SELECT *
+		        FROM " . self::$tableName . "
+		        WHERE utilisateur='" . $userId . "' AND message='" . $tweetId . "'";
+
+		return baseTable::getObject($sql,self::$objectType);
+	}
+}
+
+
+?>
