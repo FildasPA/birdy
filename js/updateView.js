2df0e75bd99089fe584c20f644537cae67e2a255
FildasPA <julien.boge@alumni.univ-avignon.fr>
2017-01-04 18:49:47 +0100

    [js] Clarify updateView.js comments

--- js/updateView.js
+++ js/updateView.js
@@ -1,18 +1,20 @@
-
 //=============================================================================
 // * Recharge une vue dans un bloc
 // ----------------------------------------------------------------------------
 // Appelle l'action passée en paramètre en AJAX et affiche son résultat
 // dans le bloc passé en second paramètre.
 // Une fois la vue rechargée, replace tous les gestionnaires d'évènements sur
-// les liens.
+// les liens internes présents dans le bloc.
 // ----------------------------------------------------------------------------
-// Notes :
-// - pourquoi ajouter les gestionnaires d'év.? tout lien affiché dans la
-// nouvelle vue ne pourra mettre à jour la vue en AJAX, et mènera directement
-// l'utilisateur à l'adresse indiquée dans 'href' => erreur.
-// - pourquoi enlever les gestionnaires d'év. déjà présents? ils s'ajouteraient
-// de manière récursive.
+// Note : pourquoi ajouter les gestionnaires d'évènements après le rechargement
+// de la vue?
+// Les liens affichés dans la nouvelle vue sont des liens normaux, c'est à dire
+// qu'ils ne font appel à aucune fonction Javascript (même s'ils sont indiqués
+// comme étant "internes" à l'aide de la classe 'ajax-nav').
+// Ainsi, au lieu de mettre à jour la vue, ces liens mèneront l'utilisateur
+// directement à l'adresse indiquée (vers le dispatcher AJAX, qui ne renvoie
+// qu'une vue seule), ce qui occasionnera la perte du layout (css, scripts,..)
+// et donc une erreur d'affichage.
 //=============================================================================
 function updateView(action,content) {
 	var address = "birdyAjax.php?action=" + action;
@@ -23,8 +25,8 @@ function updateView(action,content) {
 // * Recharge la sous-vue principale
 // ----------------------------------------------------------------------------
 // Récupère l'action (ainsi que les paramètres) se trouvant dans l'attribut
-// 'href' de l'élément courant, puis recharge la sous-vue principale dans
-// le bloc #container'.
+// 'href' du lien, puis recharge la sous-vue principale dans le bloc
+// '#container'.
 //=============================================================================
 function updateMainView(e) {
 	e.preventDefault();
@@ -32,7 +34,8 @@ function updateMainView(e) {
 }
 
 //=============================================================================
-// * Recharge la vue principale quand l'utilisateur clique sur un lien interne
+// * Recharge la vue principale lorsque l'utilisateur clique sur un lien
+// interne
 // ----------------------------------------------------------------------------
 // Place un gestionnaire d'évènement sur chaque lien interne (identifié à
 // l'aide de la classe 'ajax-nav') présent dans le bloc indiqué.
@@ -44,6 +47,9 @@ function updateMainViewOnClick(content) {
 }
 
 //=============================================================================
-// * Place un gestionnaire d'évènement sur tous les lien internes de la page.
+// * Place un gestionnaire d'évènement sur tous les liens internes
+// ----------------------------------------------------------------------------
+// Cette instruction n'est exécutée qu'une seule fois lors du chargement de la
+// page (lors du chargement du layout).
 //=============================================================================
 $(document).ready(updateMainViewOnClick(document));
