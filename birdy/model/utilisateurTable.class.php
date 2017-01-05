02f98a6d84bcaa8e995fb268ac8af9dab5e97b19
Boge Julien <uapv1502198@pedago02a.univ-avignon.fr>
2017-01-05 15:59:13 +0100

    fix upload (wip)

--- birdy/model/utilisateurTable.class.php
+++ birdy/model/utilisateurTable.class.php
@@ -58,25 +58,27 @@ class utilisateurTable extends baseTable
 	}
 
 	//---------------------------------------------------------------------------
-  // * Register
-  // Vérifie qu'aucun utilisateur existant ne possède l'identifiant souhaité,
-  // puis enregistre l'utilisateur dans la table ainsi que son avatar (dans le
-  // cas où une image a été envoyée).
-  //---------------------------------------------------------------------------
-  public static function register($request,$files)
-  {
-  	$user = new utilisateur();
+  	// * Register
+  	// Vérifie qu'aucun utilisateur existant ne possède l'identifiant souhaité,
+  	// puis enregistre l'utilisateur dans la table ainsi que son avatar (dans le
+  	// cas où une image a été envoyée).
+  	//---------------------------------------------------------------------------
+  	public static function register($request,$files)
+  	{
+  		$user = new utilisateur();
 
-  	$user->nom         = $request['name'];
-  	$user->prenom      = $request['firstname'];
-  	$user->identifiant = $request['login'];
-  	$user->pass        = sha1($request['password']);
-
+  		$user->nom         = $request['name'];
+  		$user->prenom      = $request['firstname'];
+  		$user->identifiant = $request['login'];
+  		$user->pass        = sha1($request['password']);
+	
 		if(isset($files) && isset($files['avatar']))
-			$user->avatar = self::uploadAvatar($files['avatar'],$user->identifiant);
+		$user->avatar = self::uploadAvatar($files['avatar'],$user->identifiant);
+
+		echo "uploaded!";
 
 		if(utilisateurTable::getUserByLogin($user->identifiant) === false &&
-		   $user->save())
+		   $user->save() != NULL)
 			return $user;
 
 		return false;
@@ -86,20 +88,30 @@ class utilisateurTable extends baseTable
 	// * Upload avatar
 	//------------------------------------------------------------------------------
 	public static function uploadAvatar($file,$userLogin) {
+		echo "<pre><h3>Server</h3>"; var_dump($_SERVER); echo "</pre>";
+		echo "File<br>"; var_dump($file); echo "<br>";
+		echo "identifiant<br>"; var_dump($userLogin); echo "<br>";
 		$avatar_tmp  = $file['tmp_name'];
 		$avatar_name = $file['name'];
 		$image_type  = substr($avatar_name,strrpos($avatar_name,"."));
-		$avatar_url  = $_SERVER["DOCUMENT_ROOT"] . "/Projets/birdy/images/avatars/";
-		$avatar_url .= $userLogin . $image_type;
+		$avatar_url  = $_SERVER["CONTEXT_DOCUMENT_ROOT"];
+		$avatar_url .= "/images/avatars/" . $userLogin . $image_type;
+		// $avatar_url  = $_SERVER["DOCUMENT_ROOT"] . "/Projets/birdy/images/avatars/";
+		// $avatar_url .= $userLogin . $image_type;
+
+		echo "Avatar (url)<br>"; var_dump($avatar_url); echo "<br>";
 
 		// Pour le serveur pedago (peu importe la session utilisateur) :
-		// $avatar  = $_SERVER["REQUEST_SCHEME"] . $_SERVER["SERVER_NAME"] . $_SERVER["CONTEXT_PREFIX"];
+		$avatar  = $_SERVER["REQUEST_SCHEME"] . "://" . $_SERVER["SERVER_NAME"] . $_SERVER["CONTEXT_PREFIX"];
 		// Localhost :
-		$avatar  = "http://" . $_SERVER["HTTP_HOST"] . "/Projets/birdy";
+		// $avatar  = "http://" . $_SERVER["HTTP_HOST"] . "/Projets/birdy";
 
 		$avatar .= "/images/avatars/" . $userLogin . $image_type;
 
+		echo "Avatar (bdd)<br>"; var_dump($avatar); echo "<br>";
+
 		move_uploaded_file($avatar_tmp,$avatar_url);
+		// move_uploaded_file($avatar_tmp,$avatar_url);
 
 		return $avatar;
 	}
